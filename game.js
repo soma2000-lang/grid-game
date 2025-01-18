8ad99e1a326e0885a1d93a13af898c9ec32b8022
Somasree <bishnupadamajumder32@gmail.com>
2025-01-19 05:28:25 +0530

    Adding

--- game.js
+++ game.js
@@ -1,55 +1,139 @@
 let gameIdGenerator = 1;
 
-constructor(rows, cols) {
-    this.rows=
-    this.columns=
+export default class Game {
+  constructor(rows, cols) {
+    this.gameId = gameIdGenerator++;
+    this.rows = rows;
+    this.cols = cols;
     this.grid = Array.from({ length: rows }, () => Array(cols).fill("_"));
     this.players = [];
     this.step = 0;
     this.winningPosition = {
-        x: Math.floor(Math.random() * rows),
-        y: Math.floor(Math.random() * cols),
-      };
+      x: Math.floor(Math.random() * rows),
+      y: Math.floor(Math.random() * cols),
+    };
     this.grid[this.winningPosition.x][this.winningPosition.y] = "X";
+  }
 
-}
-addPlayer(player) {
+  static create(rows, cols) {
+    return new Game(rows, cols);
+  }
+
+  addPlayer(player) {
     player.placePlayer(this.rows, this.cols, this.grid);
     this.players.push(player);
     this.grid[player.xCoordinate][player.yCoordinate] = player.id;
   }
 
-static create(rows, cols) {
-    return new Game(rows, cols);
-  }
-
-
-  
-  static create(rows, cols) {
-    return new Game(rows, cols);
-  }
-  playNextStep() {
-    this.playNextStep()++;
-  }
-check win()
-{}
-start() {
+  start() {
    
     this.playNextStep();
   }
-displayGrid() {
-}
-checkCollision()
-{}
 
+  playNextStep() {
+    this.step++;
 
-start() {
-    //console.log(`\nGame[${this.gameId}]:\n`);
-    //this.displayGrid();
-    // console.log(
-    //   `Game ${String(this.gameId).padStart(3, "0")} winning coordinate (${
-    //     this.winningPosition.x
-    //   },${this.winningPosition.y})\n`
-    // );
-    this.playNextStep();
+    console.log(
+      `Game ${String(this.gameId).padStart(3, "0")} Turn ${String(
+        this.step
+      ).padStart(3, "0")}:\n`
+    );
+
+   
+    this.players.forEach((player) => {
+      player.nextMove(this.grid, this.winningPosition);
+    });
+
+    if (this.checkWin()) {
+      console.log(`Game ${String(this.gameId).padStart(3, "0")} Over!\n`);
+      return;
+    }
+
+    if (this.checkCollision()) {
+      console.log(`Game ${String(this.gameId).padStart(3, "0")} Over!\n`);
+      return;
+    }
+
+    this.displayGrid();
+
+    setTimeout(() => {
+      this.playNextStep();
+    }, "5000");
   }
+
+  checkCollision() {
+    const positions = new Map();
+    let collisionDetected = false;
+    let [firstPlayer, secondPlayer] = ["", ""];
+
+    this.players.forEach((player) => {
+      if (player.isCollitionOccurred) {
+        return true;
+      }
+
+      const position = `${player.xCoordinate},${player.yCoordinate}`;
+
+      if (!positions.has(position)) {
+        positions.set(position, []);
+      }
+
+      positions.get(position).push(player);
+    });
+
+    positions.forEach((players) => {
+      if (players.length > 1) {
+        if (!collisionDetected) {
+          console.log(`Collision Detected!\n`);
+          collisionDetected = true;
+        }
+
+        players.forEach((player) => {
+          player.isCollitionOccurred = true;
+          
+        });
+      }
+    });
+
+    return collisionDetected;
+  }
+
+  displayGrid() {
+
+    for (let i = 0; i < this.rows; i++) {
+      let rowString = "";
+      for (let j = 0; j < this.cols; j++) {
+        rowString += this.grid[i][j] + " ";
+      }
+      console.log(rowString.trim());
+    }
+    console.log("\n");
+  }
+
+  checkWin() {
+    let onlyOnePlayerWinCheck = 0;
+    let winPlayerId = "";
+
+    for (let player of this.players) {
+      if (
+        player.xCoordinate === this.winningPosition.x &&
+        player.yCoordinate === this.winningPosition.y
+      ) {
+        onlyOnePlayerWinCheck++;
+        winPlayerId = player.id;
+      }
+    }
+
+    if (onlyOnePlayerWinCheck == 1) {
+      this.displayGrid();
+      console.log(
+        `Player ${winPlayerId} wins the Game ${String(this.gameId).padStart(
+          3,
+          "0"
+        )} at position (${this.winningPosition.x}, ${this.winningPosition.y})\n`
+      );
+      return true;
+    }
+
+    return false;
+  }
+}
---
+++ player.js
@@ -0,0 +1,59 @@
+let playerIdGenerator = 0;
+
+export default class Player {
+  constructor() {
+    this.id = String.fromCharCode(65 + playerIdGenerator++);
+    this.xCoordinate = 0;
+    this.yCoordinate = 0;
+    this.isCollitionOccurred = false;
+  }
+
+  static create() {
+    return new Player();
+  }
+
+  placePlayer(rows, cols, grid) {
+    let x, y;
+
+    do {
+      x = Math.floor(Math.random() * rows);
+      y = Math.floor(Math.random() * cols);
+    } while (grid[x][y] !== "_");
+
+    this.xCoordinate = x;
+    this.yCoordinate = y;
+
+    // console.log(
+    //   `Player [${this.id}] added at (${this.xCoordinate},${this.yCoordinate}).`
+    // );
+  }
+
+  nextMove(grid, winningPosition) {
+    if (this.isCollitionOccurred) {
+      return;
+    }
+    if (
+      this.xCoordinate === winningPosition.x &&
+      this.yCoordinate === winningPosition.y
+    ) {
+      return;
+    }
+
+    if (grid[this.xCoordinate][this.yCoordinate] === this.id) {
+      grid[this.xCoordinate][this.yCoordinate] = "_";
+    }
+
+    const dx = winningPosition.x - this.xCoordinate;
+    const dy = winningPosition.y - this.yCoordinate;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+      this.xCoordinate += Math.sign(dx);
+    } else {
+      this.yCoordinate += Math.sign(dy);
+    }
+
+
+    grid[this.xCoordinate][this.yCoordinate] = this.id;
+    
+  }
+}
